d6ba9aa4b34deadd3e4f41cae7d84014b6498d1f
Julien BONTRON <bontronjulien73@gmail.com>
2016-04-07 09:00:17 +0200

    Ajout de l'interface graphique qui ne fonctionne pas encore

--- src/reverso/Reverso.java
+++ src/reverso/Reverso.java
@@ -6,6 +6,10 @@ import jeu.core.Jeu;
 public class Reverso {
 
     public static void main(String[] args) {
+        // lancer de la fenetre
+        Fenetre fen = new Fenetre();
+        
+        // Jeu en mode console qui fonctionne
         Jeu j = new Jeu();
         Clavier c = new Clavier();
         j.start(c);
