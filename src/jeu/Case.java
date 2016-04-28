754ecc48c15999e5bf3689d27322ea684b791a9a
lilianvarrel-tacos <lilianvarrel@hotmail.fr>
2016-04-28 10:57:22 +0200

    Commentaire sur la partie core

--- src/jeu/Case.java
+++ src/jeu/Case.java
@@ -2,9 +2,13 @@ package jeu;
 
 public interface Case {
     
-    public boolean remplie();   // Indique si la case est prise par un joueur
-    public boolean blanche();   // Si la case est remplie, indique si c'est par le joueur blanc (second joueur)
-                                // Sinon, lève une exception
-    public boolean jouable();   // Indique s'il est possible de jouer sur cette case
+    // Indique si la case est prise par un joueur
+    public boolean remplie();
     
+    // Si la case est remplie, indique si c'est par le joueur blanc
+    //   (second joueur), sinon lève une exception
+    public boolean blanche();
+    
+    // Indique s'il est possible de jouer sur cette case
+    public boolean jouable();
 }
--- src/jeu/core/TableauCase.java
+++ src/jeu/core/TableauCase.java
@@ -5,6 +5,7 @@ public class TableauCase{
     private int dimY;
     private CaseR[] tab;
     
+    // Crée un damier et le rempli les 4 cases du centre
     public TableauCase(int dimY, int dimX) {
         this.dimX = dimX;
         this.dimY = dimY;
@@ -19,7 +20,6 @@ public class TableauCase{
                 case1 = new CaseR();
             tab[i] = case1;
         }
-        
     }
     
     public int getDimY() {
@@ -40,6 +40,9 @@ public class TableauCase{
         this.tab = tab;
     }
     
+    // Parcours toutes les cases du tableau, et met à true
+    // le boolean joauble sur les cases jouables
+    // Paramètre joueur : vrai si blanc doit jouer, faux si au tour noir
     public boolean chercherCase(boolean joueur){
         boolean fini = true;
         for (int x =0; x<dimX;x++)
@@ -54,18 +57,27 @@ public class TableauCase{
         return fini;
     }
     
+    // Fonction de test des conditions, renvoie true si la case passée en
+    // paramètre est sur le damier et contient un pion
     public boolean nonVide(int cx, int cy){
         return (cx>=0&&cx<dimX&&cy>=0&&cy<dimY&&!getCase(cx,cy).isVide());
     }
     
+    // Recherche si une case est jouable, test tous les vecteurs aux
+    // alentours, renvoie vrai si le joueur (true = blanc) peut jouer
     public boolean peutJouer(int x, int y, boolean joueur){
+        // On initialise le vecteur
         int cx = x-1;
         int cy = y-1;
+        // Si le pion à coté de la case est un pion adverse...
         if(nonVide(cx,cy)&&getCase(cx,cy).isBool(!joueur)){
+            // Temps qu'il y a des pions adverses on se déplace
             while(nonVide(cx,cy)&&getCase(cx,cy).isBool(!joueur)){
                 cx--;
                 cy--;
             }
+            // Si après les pions adverses il y a un pion du joueur,
+            // on peut manger !
             if(nonVide(cx, cy)&&getCase(cx,cy).isBool(joueur))
                 return true;
         }
@@ -146,7 +158,9 @@ public class TableauCase{
                 cx--;
                 cy--;
             }
+            // Si on peut manger de ce coté :
             if(nonVide(cx, cy)&&getCase(cx,cy).isBool(joueur)){
+                // On mange !!!
                 cx = choix[0]-1;
                 cy = choix[1]-1;
                 do{
@@ -268,6 +282,7 @@ public class TableauCase{
         getCase(choix[0],choix[1]).jouer(joueur);
     }
     
+    // Renvoie true si au moins une case du plateau est jouable
     public boolean caseJouable(){
         for (int x =0; x<dimX;x++)
             for (int y =0; y<dimX;y++)
@@ -276,6 +291,9 @@ public class TableauCase{
         return false;
     }
     
+    // Compte les points en fin de partie.
+    // Si des cases sont vides, elles appartiennent au dernier joueur 
+    // qui a posé un pion (true pour blanc, false pour noir)
     public int comptePoints(boolean joueur){
         int c = 0;
         for (int x =0; x<dimX;x++)
@@ -294,6 +312,7 @@ public class TableauCase{
         return copie;
     }
     
+    // Copie l'objet actuel
     public TableauCase copieTout(){
         TableauCase copie;
         copie = new TableauCase(dimY, dimX);
@@ -301,6 +320,7 @@ public class TableauCase{
         return copie;
     }
     
+    // Compte les cases vides (comme le nom l'indique)
     public int compteCasesVides(){
         int ret = 0;
         for(int i=0;i<63;i++)
