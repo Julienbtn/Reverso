b573f56602192ac31b1bfa3c9bcc444f46efc893
lilianvarrel-tacos <lilianvarrel@hotmail.fr>
2016-04-28 17:13:07 +0200

    Modif calcul des points IA Lilian

--- src/jeu/ia/IntelligenceValuationMaxIA.java
+++ src/jeu/ia/IntelligenceValuationMaxIA.java
@@ -2,6 +2,8 @@ package jeu.ia;
 
 import static java.lang.Math.abs;
 import java.util.ArrayList;
+import java.util.Random;
+import jeu.Case;
 import jeu.Plateau;
 
 public class IntelligenceValuationMaxIA extends IntelligenceBase{
@@ -19,14 +21,13 @@ public class IntelligenceValuationMaxIA extends IntelligenceBase{
         // On crée un patron correspondant au coin supérieur gauche
         int[] patron = new int[16];
         patron[0]=1000;//angle
-        patron[1]=patron[4]=-50;//bord contre angle
-        patron[2]=patron[8]=30;//bord proche angle
-        patron[3]=patron[12]=15;//bord centre
-        patron[5]=-150;//diag contre angle
-        patron[6]=patron[7]=patron[9]=patron[13]=0;//case proche bord
-        patron[10]=1;
-        patron[11]=patron[14]=2;
-        patron[15]=5;//cases du centre
+        patron[1]=patron[4]=-75;//bords contres angle
+        patron[2]=patron[8]=20;//bords proches angle
+        patron[3]=patron[12]=8;//bords centre
+        patron[5]=-200;//diag contre angle
+        patron[6]=patron[7]=patron[9]=patron[13]=-10;//cases proches bord
+        patron[10]=patron[11]=patron[14]=2;//cases autour centre
+        patron[15]=20;//case du centre
         //On place le patron dans la grille, en le retournant si nécessaire
         for(int i=0;i<8;i++)
             for(int j=0;j<8;j++)
@@ -45,70 +46,69 @@ public class IntelligenceValuationMaxIA extends IntelligenceBase{
         return grille;
     }
     
-    // Calcul les points qu'à l'IA sur un plateau
+    // Calcul les points qu'à l'IA en utilisant la grille
     // Si il y a 4 cases libres ou moints, on calcule le nombre de pions
     public int calculerPoints(Plateau plate,boolean blanc){
         int score = 0;
-        if(plate.nbCasesLibres()>4){
-            for(int i= 0;i<64;i++)
-                if(plate.getDamier()[i].remplie())
-                    if(plate.getDamier()[i].blanche()==blanc)
-                        score+=valeurs[i];
+        // Si le joueur possède un angle les cases autour sont avantageuses
+        boolean hautgauche,hautdroit,basgauche,basdroit;
+        hautgauche=possedeCase(plate.getDamier()[0],blanc);
+        hautdroit=possedeCase(plate.getDamier()[7],blanc);
+        basgauche=possedeCase(plate.getDamier()[56],blanc);
+        basdroit=possedeCase(plate.getDamier()[63],blanc);
+        // Si l'angle est pris la valeur de la case à coté en ligne
+        // devient abs(case diag) et inversement
+        int valdiag =abs(valeurs[9]);
+        int valligne=abs(valeurs[1]);
+        // Si ce n'est pas les 2 derniers coups on regarde la grille
+        if(plate.nbCasesLibres()>3){
+            for(int i= 0;i<64;i++){
+                if(possedeCase(plate.getDamier()[i],blanc)){
+                     switch(i){
+                        case 1:case 8:score+=(hautgauche?valdiag:valeurs[i]);break;
+                        case 9:score+=(hautgauche?valligne:valeurs[i]);break;
+                        case 6:case 15:score+=(hautdroit?valdiag:valeurs[i]);break;
+                        case 14:score+=(hautdroit?valligne:valeurs[i]);break;
+                        case 48:case 57:score+=(basgauche?valdiag:valeurs[i]);break;
+                        case 49:score+=(basgauche?valligne:valeurs[i]);break;
+                        case 55:case 62:score+=(basdroit?valdiag:valeurs[i]);break;
+                        case 54:score+=(basdroit?valligne:valeurs[i]);break;
+                        default:score+=valeurs[i];break;
+                    }
+                }
+            }
         }
-        else { //2 derniers coups de l'IA
+        else { //Sinon on compte les pions
             score = (blanc?plate.scoreBlanc():plate.scoreNoir());
         }
         return score;
     }
     
-    // Si un angle est pris, on change les valeurs des cases adjacentes
-    public void coinPris(int angle){
-        // Si on prend un angle, on change la valeur des cases adjacentes,
-        // en prenant en priorité les cases sur le bord.
-        int valdiag =abs(valeurs[9]);
-        int valligne=abs(valeurs[1]);
-        if(angle==0){
-            valeurs[1]=valeurs[8]=valdiag;
-            valeurs[9]=valligne;
-        }
-        else if(angle==7){
-            valeurs[6]=valeurs[15]=valdiag;
-            valeurs[14]=valligne;
-        }
-        else if(angle==56){
-            valeurs[48]=valeurs[57]=valdiag;
-            valeurs[49]=valligne;
-        }
-        else if(angle==63){
-            valeurs[62]=valeurs[55]=valdiag;
-            valeurs[54]=valligne;
-        }
+    // Renvoie vrai si le joueur possède la case
+    public boolean possedeCase(Case c, boolean joueur){
+        return c.remplie() && c.blanche()==joueur;
     }
-    
+        
     // L'ia renvoie la case sur laquelle elle va jouer
     @Override
     public int mouvement() throws NoFreeCaseException {
+        Random rnd  = new Random();
         Plateau copie;
         int scoreIA;
-        int[] meilleurCoup = new int[2];
-        meilleurCoup[1]=-3000;
-        ArrayList<Integer> jouables;
-        jouables = casesJouables();
+        int meilleurCoup=-30000;
+        ArrayList<Integer> positionsMeilleurCoup = new ArrayList<>();
+        ArrayList<Integer> jouables= casesJouables();
         for(int i=0;i<jouables.size();i++){
             copie =simuler(plateau, jouables.get(i));
             scoreIA= calculerPoints(copie,plateau.tourBlanc());
-            if(scoreIA>meilleurCoup[1]){
-                meilleurCoup[1] = scoreIA;
-                meilleurCoup[0] = i;
+            if(scoreIA==meilleurCoup)
+                positionsMeilleurCoup.add(jouables.get(i));
+            else if(scoreIA>meilleurCoup){
+                meilleurCoup = scoreIA;
+                positionsMeilleurCoup.clear();
+                positionsMeilleurCoup.add(jouables.get(i));
             }
         }
-        // Si elle joue sur un angle on met à jour les valeurs des cases
-        switch(jouables.get(meilleurCoup[0])){
-            case 0: coinPris(0);break;
-            case 7: coinPris(7);break;
-            case 56: coinPris(56);break;
-            case 63: coinPris(63);break;
-        }
-        return jouables.get(meilleurCoup[0]);
+        return positionsMeilleurCoup.get(rnd.nextInt(positionsMeilleurCoup.size()));
     }
 }
